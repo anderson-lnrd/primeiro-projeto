956cb9122f5d621540f5a01a118874dd4b76e125
Anderson Leonardo <anderson.ldsilv@gmail.com>
2022-10-23 21:47:20 -0300

    back-end introduction with NodeJs

---
+++ src/main.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send("app GET")
+})
+
+app.post('/', (req, res) => {
+    res.send("app POST")
+})
+
+app.delete('/', (req, res) => {
+    res.send("app DELETE")
+})
+
+app.put('/', (req, res) => {
+    res.send("app PUT")
+})
+
+app.listen(PORT, () => {
+    console.log(`App online na porta ${PORT}`);
+});
